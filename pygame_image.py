1ab3c7e46117cf6520f6ee9f109609338d8248dd
koma <c0b23070c6@edu.teu.ac.jp>
2024-06-11 16:43:57 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -33,6 +33,20 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, 1)
+
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1, 0)
+
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
