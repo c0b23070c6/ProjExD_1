ef3f6e96d7a128f2658f7abecab2040f02f88ea0
koma <c0b23070c6@edu.teu.ac.jp>
2024-06-11 16:12:42 +0900

    練習4

--- pygame_image.py
+++ pygame_image.py
@@ -10,16 +10,23 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    tmr = 0
-
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect() #こうかとんrectの抽出
+    kk_rct.center = 300, 200
+    tmr = 0
+
+
+
+
+
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
